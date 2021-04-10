a89f0ba158f08ac6e1fa40f1fe05da53b1e0064a
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-04-10 16:33:22 +0300

    Rename app file

--- app/src/app.py
+++ app/src/app.py
@@ -52,9 +52,6 @@ def page_not_found(e):
 def server_error(e):
     return render_template('500.html'), 500
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
-
 def main():
     app.run(host='0.0.0.0', port=8080)
 
