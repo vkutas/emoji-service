0ae369df5fca4a520c062c5981760c487ff4b15e
Vladislav Kutas <kutas.uladzislau@gmail.com>
2021-04-11 08:55:31 +0300

    Fix pylint errors

--- app/src/app.py
+++ app/src/app.py
@@ -1,8 +1,7 @@
+import random
 from flask import Flask, render_template
 from flask import request
 import emoji
-import random
-
 
 app = Flask(__name__)
 
@@ -16,11 +15,12 @@ def say_hello():
 
 @app.route('/', methods=['GET', 'POST'])
 def emoji_service():
+    
     if request.method == 'POST':
         content = request.get_json(force=True)
         return emoji_provider(content)
-    else:
-        return render_template('greeting.html', url = request.url)
+    
+    return render_template('greeting.html', url = request.url)
 
 def emoji_provider(emoji_desc):
     """ Take a dict of format {word: 'name', count: n} and
@@ -41,21 +41,22 @@ def emoji_provider(emoji_desc):
         emoji_icon = random.choice(list(emoji.EMOJI_UNICODE_ENGLISH.values()))
     if not isinstance(count, int):
         count = random.randint(1, 10)
-    response_body = emoji_icon    
+    response_body = emoji_icon  
     for i in range(count):
         response_body += name + emoji_icon
     return response_body
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(error_code):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def server_error(e):
+def server_error(error_code):
     return render_template('500.html'), 500
 
 def main():
     app.run(host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
+
     main()
